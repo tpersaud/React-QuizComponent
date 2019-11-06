ed97c37d3eb6c510c6b1af86fba234cd1105fe2a
Terrence Persaud Jr <terrence.persaud@gmail.com>
2019-11-06 03:00:18 -0600

    finished module 7

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,13 +16,19 @@ class Quiz extends Component {
     })
   }
 
+  handleResetClick(){
+    this.setState(() => {
+      return { quiz_position: 1 }
+    })
+  }
+
   render() {
     const isQuizEnd = this.state.quiz_position -1 === quizData.quiz_questions.length;
     return(
       <div>
         {
           isQuizEnd ? 
-          <QuizEnd /> : 
+          <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} /> :
           <QuizQuestion 
             quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
             showNextQuestionHandler={this.showNextQuestion.bind(this)}
