7dd74f2faa9680b5bae2337e5077a68b1376bc4f
Terrence Persaud Jr <terrence.persaud@gmail.com>
2019-11-06 02:44:57 -0600

    finished module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,6 +10,12 @@ class Quiz extends Component {
     this.state = { quiz_position:1 }
   }
 
+  showNextQuestion(){
+    this.setState((state) => {
+      return { quiz_position: state.quiz_position + 1 }
+    })
+  }
+
   render() {
     const isQuizEnd = this.state.quiz_position -1 === quizData.quiz_questions.length;
     return(
@@ -17,7 +23,10 @@ class Quiz extends Component {
         {
           isQuizEnd ? 
           <QuizEnd /> : 
-          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} />
+          <QuizQuestion 
+            quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
+          />
         }
       </div>
   )}
